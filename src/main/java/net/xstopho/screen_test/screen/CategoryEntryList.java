b2b777b9cd2e7cb834d16e30083b5d07b7a6bd65
Stein-N <nico-stein-98@outlook.de>
2024-08-08 19:52:27 +0200

    remove CategoryTab from CategoryEntryList

--- src/main/java/net/xstopho/screen_test/screen/CategoryEntryList.java
+++ src/main/java/net/xstopho/screen_test/screen/CategoryEntryList.java
@@ -3,14 +3,12 @@ package net.xstopho.screen_test.screen;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.ContainerObjectSelectionList;
+import net.minecraft.client.gui.screens.Screen;
 
 public class CategoryEntryList extends ContainerObjectSelectionList<CategoryEntry> {
 
-    private final CategoryTab tab;
-
-    public CategoryEntryList(CategoryTab tab, Minecraft minecraft, int width, int contentHeight, int headerHeight, int itemSpacing, Iterable<CategoryEntry> entries) {
+    public CategoryEntryList(Minecraft minecraft, int width, int contentHeight, int headerHeight, int itemSpacing, Iterable<CategoryEntry> entries) {
         super(minecraft, width, contentHeight, headerHeight, itemSpacing);
-        this.tab = tab;
         entries.forEach(this::addEntry);
     }
 
@@ -26,6 +24,7 @@ public class CategoryEntryList extends ContainerObjectSelectionList<CategoryEntr
 
     @Override
     public int getRowWidth() {
-        return this.width - 120;
+        Screen screen = Minecraft.getInstance().screen;
+        return this.width - (screen.width / 4);
     }
 }
