972ed76c52831a1574271029c42fc43fadac6dda
Stein-N <nico-stein-98@outlook.de>
2024-08-11 01:05:46 +0200

    finishing enum support

---
+++ src/main/java/net/xstopho/screen_test/screen/entries/EnumSelectionEntry.java
@@ -0,0 +1,43 @@
+package net.xstopho.screen_test.screen.entries;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.EditBox;
+import net.minecraft.client.gui.components.Tooltip;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+import net.xstopho.screen_test.screen.entries.base.BaseEntry;
+
+public class EnumSelectionEntry<T extends Enum<T>> extends BaseEntry {
+
+    private final Button valueButton;
+
+    public EnumSelectionEntry(Screen previous, Component entryLabel, Component entryTooltip, EditBox parentBox, Enum<T> value) {
+
+        this.valueButton = Button.builder(entryLabel, b -> {
+            Minecraft.getInstance().setScreen(previous);
+            parentBox.setValue(value.toString());
+        }).tooltip(Tooltip.create(entryTooltip)).width(150).build();
+
+        this.children.add(valueButton);
+    }
+
+    @Override
+    public void render(GuiGraphics guiGraphics, int index, int yPos, int xPos, int entryWidth, int entryHeight,
+                       int mouseX, int mouseY, boolean hovered, float partialTick) {
+        valueButton.setX(xPos + (entryWidth / 2) - 75);
+        valueButton.setY(yPos);
+
+        valueButton.render(guiGraphics, mouseX, mouseY, partialTick);
+    }
+
+    @Override
+    public void saveChangedValue() {}
+
+    @Override
+    public void undoChanges() {}
+
+    @Override
+    public void resetValues() {}
+}
--- src/main/java/net/xstopho/screen_test/screen/entries/EnumValueEntry.java
+++ src/main/java/net/xstopho/screen_test/screen/entries/EnumValueEntry.java
@@ -3,17 +3,23 @@ package net.xstopho.screen_test.screen.entries;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.EditBox;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import net.xstopho.screen_test.config.TestConfigEntry;
-import net.xstopho.screen_test.helper.ConfigEntryCreator;
 import net.xstopho.screen_test.screen.SingleSelectionScreen;
+import net.xstopho.screen_test.screen.entries.base.BaseEntry;
 import net.xstopho.screen_test.screen.entries.base.ValueEntry;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
 
 public class EnumValueEntry<T extends Enum<T>> extends ValueEntry<T> {
 
     private final TestConfigEntry.EnumConfigEntry<T> entry;
     private final EditBox editBox;
+    private final Screen currentScreen = Minecraft.getInstance().screen;
 
     public EnumValueEntry(Component entryLabel, @Nullable Component entryTooltip, TestConfigEntry.EnumConfigEntry<T> entry) {
         super(entryLabel, entryTooltip);
@@ -36,13 +42,24 @@ public class EnumValueEntry<T extends Enum<T>> extends ValueEntry<T> {
         editBox.setX(xPos + entryWidth - getValueWidgetWidth());
         editBox.setY(yPos + 1);
         editBox.setWidth(getValueWidgetWidth() - (undoButton.getWidth() + resetButton.getWidth()) - 1);
+        this.editBox.setResponder(string -> setUndoState(!Objects.equals(string, entry.getConfigValue().toString())));
 
         editBox.render(guiGraphics, mouseX, mouseY, partialTick);
 
         if (editBox.isFocused()) {
             editBox.setFocused(false);
-            Minecraft.getInstance().setScreen(new SingleSelectionScreen(Minecraft.getInstance().screen, this.entryLabel, ConfigEntryCreator.createDummies()));
+            Minecraft.getInstance().setScreen(new SingleSelectionScreen(currentScreen, this.entryLabel, createEntries()));
         }
+    }
+
+    private List<BaseEntry> createEntries() {
+        List<BaseEntry> list = new LinkedList<>();
+
+        for (Enum<T> value : entry.getEnumValues()) {
+            list.add(new EnumSelectionEntry<>(currentScreen, Component.literal(value.toString()), Component.literal("Description for " + value), editBox, value));
+        }
+
+        return list;
     }
 
     @Override
